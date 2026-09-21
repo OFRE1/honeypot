0c010aa2fd9c9550ce2de63c5c6a8d57a3010f5b
OFRE1 <tranthuyvph+12@gmail.com>
2026-09-21 14:13:53 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,6 +5,8 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import threading
+
 
 
 class HealthCheck(BaseHTTPRequestHandler):
